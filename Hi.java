4f39cea43c274999e85b6b309848fdcc0a2ac44f
shahzad-nazeer <40621982+shahzad-nazeer@users.noreply.github.com>
2026-01-04 22:36:06 +0500

    Update Hi.java

--- Hi.java
+++ Hi.java
@@ -3,5 +3,7 @@ public class Hi{
      System.out.println("Hi Every 1");
      System.out.println("Welcome in Java Git Hub Example");
      System.out.println("Add More Data to verify jenkins Run");    
+     System.out.println("Call Java From Jenkins Server");    
+     
   }
 }
